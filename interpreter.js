b1fee7462a5295dbc568c5df4e654e7627e97da5
world-wide-web-1 <105808883+world-wide-web-1@users.noreply.github.com>
2024-09-18 14:51:48 -0400

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1817,7 +1817,7 @@ async function executeBaCoLa(src, level) {
 
 var init = async () => {
   try {
-    var data = await fetch("https://world-wide-web-1.github.io/BaCoLa-HTML/language");
+    var data = await fetch("language");
     var program = await data.text();
     let _l = compileLines(program, syntaxoptions);
     for (let [i, line] of _l.entries()) {
